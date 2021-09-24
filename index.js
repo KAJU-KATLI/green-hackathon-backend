8e5daec621e588b8f5014fbd9a8aabf6e4152d6d
Naman Malhotra <namalhotra333@gmail.com>
2021-09-24 16:59:21 +0530

    small changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const connectDB = require("./db/db");
-require('dotenv').config("./.env")
 connectDB();
 
 const app = express();
-app.use(express.json());
 const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
