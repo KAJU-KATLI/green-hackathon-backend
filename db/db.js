5bb685359fd9d99e5f3b42aa85992e1b668eb9d1
Vaibhav Gupta <guptavaibhav432@gmail.com>
2021-09-24 13:30:41 +0530

    MongoUrl and Folders added

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const URL = "";
+const URL = "mongodb+srv://greenhackathon:greenhackathon@cluster0.utrzu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const connectDB = () => {
   mongoose.connect(URL, () => {
